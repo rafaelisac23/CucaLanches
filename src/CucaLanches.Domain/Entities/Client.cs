b52eb89b6788a930f7723874d0c4ffe217e2bbb2
Rhuaannn <106773136+rhuaannn@users.noreply.github.com>
2026-09-05 22:23:41 -0300

    Make Client properties have private setters
    Changed properties in Client class to have private setters for encapsulation.

--- src/CucaLanches.Domain/Entities/Client.cs
+++ src/CucaLanches.Domain/Entities/Client.cs
@@ -1,11 +1,12 @@
 namespace CucaLanches.Domain.Entities;
 
-public class Client
+public sealed class Client
 {
-    public int id { get; set; }
-    public required string Name { get; set; }
-    public required string Phone { get; set; }
-    public string? Email { get; set; }
-    public DateTime CreatedAt { get; set; }
+    public int id { get; private set; }
+    public required string Name { get; private set; }
+    public required string Phone { get; private set; }
+    public string? Email { get; private set; }
+    public DateTime CreatedAt { get; private set; }
     public List<Address> Addresses { get; set; } = [];
+
 }
